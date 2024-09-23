d06fcc505886257524c25a8f9e3fe81738ad6778
Mike Kristensen <mhkr@econ.au.dk>
2024-09-23 15:50:10 +0200

    removed duplicate var

--- ipl/ipl/settings.py
+++ ipl/ipl/settings.py
@@ -63,8 +63,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = 'ipl.urls'
 
